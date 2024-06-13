9f16e748272a8ebdcc880174ef1ece0848b23bd2
Yurii Hlushenko <yauglushenko@gmail.com>
2024-06-13 12:02:59 +0300

    arrows position fixed

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,29 @@
-module.exports = {
-  extends: "@mate-academy/stylelint-config",
-  plugins: [
-    "stylelint-scss"
+{
+  "customSyntax": "postcss-html",
+  "extends": [
+    "stylelint-config-html",
+    "stylelint-config-recommended-scss",
+    "stylelint-config-recommended-vue/scss",
+    "stylelint-prettier/recommended"
   ],
-  rules: {}
-};
+  "overrides": [
+    {
+      "files": ["**/*.vue"],
+      "customSyntax": "postcss-scss"
+    }
+  ],
+  "plugins": ["stylelint-order", "stylelint-prettier"],
+  "rules": {
+    "declaration-empty-line-before": "never",
+    "font-family-name-quotes": "always-unless-keyword",
+    "order/order": ["custom-properties", "declarations"],
+    "order/properties-alphabetical-order": true,
+    "prettier/prettier": true,
+    "selector-class-pattern": [
+      "/^[a-z]+(?:-[a-z]+)*(?:_{2}[a-z]+(?:-[a-z]+)*)?(?:-{2}[a-z]+(?:-[a-z]+)*)?$/",
+      {
+        "resolveNestedSelectors": true
+      }
+    ]
+  }
+}
